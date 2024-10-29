8d8a35f9a38946a296bda322bc056f5efcded5cc
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2024-10-29 18:30:07 -0300

    feat: change MAX_CONTENT_LENGTH

--- app_v2.py
+++ app_v2.py
@@ -5,23 +5,22 @@ import time
 from flask import Flask, request, send_file, jsonify, render_template, abort
 from werkzeug.utils import secure_filename
 from video_stacker import stack_videos_vertically_with_loop
+import traceback
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'data'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024  # Limite de 500MB por upload
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 * 1024  # Limite de 10GB por upload
 
 # Listas para armazenar os arquivos "source" e vídeos processados
 source_files = []
 processed_videos = []
 
-
 @app.route('/')
 def index():
     return render_template('index_v2.html')
-
 
 @app.route('/upload_videos', methods=['POST'])
 def upload_videos():
@@ -41,12 +40,16 @@ def upload_videos():
     output_filename = f"out_{uuid.uuid4()}.mp4"
     output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
 
-    stack_videos_vertically_with_loop(video1_path, video2_path, output_path)
-    processed_videos.append(output_filename)
+    try:
+        stack_videos_vertically_with_loop(video1_path, video2_path, output_path)
+        processed_videos.append(output_filename)
+    except Exception as e:
+        print(f"Erro ao processar os vídeos: {str(e)}")
+        print(traceback.format_exc())
+        return jsonify({'error': f'Erro ao processar os vídeos: {str(e)}'}), 500
 
     # Retorna a URL direta para o vídeo processado
     return jsonify({'video_url': f'/videos/{output_filename}'}), 200
-
 
 @app.route('/finalize_uploads', methods=['POST'])
 def finalize_uploads():
@@ -58,8 +61,9 @@ def finalize_uploads():
         source_files.clear()  # Limpa a lista após remover os arquivos
         return jsonify({'message': 'Arquivos source removidos com sucesso.'}), 200
     except Exception as e:
+        print(f"Erro ao remover arquivos: {str(e)}")
+        print(traceback.format_exc())
         return jsonify({'error': f'Erro ao remover arquivos: {str(e)}'}), 500
-
 
 @app.route('/download_zip')
 def download_zip():
@@ -70,13 +74,17 @@ def download_zip():
     zip_path = os.path.join(app.config['UPLOAD_FOLDER'], zip_filename)
 
     # Cria o ZIP com os vídeos processados
-    with zipfile.ZipFile(zip_path, 'w') as zipf:
-        for filename in processed_videos:
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            zipf.write(file_path, filename)
+    try:
+        with zipfile.ZipFile(zip_path, 'w') as zipf:
+            for filename in processed_videos:
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                zipf.write(file_path, filename)
+    except Exception as e:
+        print(f"Erro ao criar o arquivo ZIP: {str(e)}")
+        print(traceback.format_exc())
+        return jsonify({'error': f'Erro ao criar o arquivo ZIP: {str(e)}'}), 500
 
     return send_file(zip_path, as_attachment=True)
-
 
 @app.route('/videos/<filename>')
 def serve_video(filename):
@@ -85,7 +93,6 @@ def serve_video(filename):
         abort(404)
 
     return send_file(file_path, as_attachment=True)
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
