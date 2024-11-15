1098f49613ac64b2b460bbf2d7fafde3ec4fc220
Ildefonso Zanette <izanette@gmail.com>
2024-11-15 18:29:57 -0300

    new function to generate a random string

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 import time
 import logging
+import random
+import string
 
 logger = logging.getLogger(__name__)
+
 
 def generate_datetime_filename(prefix="file", extension="txt"):
     # Format the current date and time
@@ -10,6 +13,7 @@ def generate_datetime_filename(prefix="file", extension="txt"):
     # Construct the filename
     filename = f"{prefix}_{current_time}.{extension}"
     return filename
+
 
 # timer decorator
 # use like this:
@@ -25,3 +29,15 @@ def timer(func):
         logger.info(f"{func.__name__} took {end_time - start_time:.4f} seconds")
         return result
     return wrapper
+
+
+def generate_random_string(length):
+    if length <= 0:
+        raise ValueError("Length must be a positive integer.")
+
+    # Define characters safe for filenames and URLs
+    allowed_chars = string.ascii_letters + string.digits + "-_"
+
+    # Generate a random string
+    random_string = ''.join(random.choices(allowed_chars, k=length))
+    return random_string
